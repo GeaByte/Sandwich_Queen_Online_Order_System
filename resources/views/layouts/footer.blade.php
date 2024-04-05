b3b3c2bf4ef32898a3a99facfca39cb04fdd75d1
Li Hsin Lin <xxxamfa@gmail.com>
2024-04-05 14:35:12 -0700

    fix footer style

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
 <div class="copyright py-4 text-center text-white">
     <div class="container">
         <small>
-            Li Hsin Lin - 300368867<br>
+            Li Hsin Lin – 300368867<br>
             Raymond Wang – 300374626
         </small>
     </div>
