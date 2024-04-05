3c68bf29097c8952da94b897c9673a8089640ef5
Li Hsin Lin <xxxamfa@gmail.com>
2024-04-05 15:11:48 -0700

    change db name add admin user

---
+++ sandwich.sql
@@ -0,0 +1,10 @@
+USE sandwichQueen
+INSERT INTO users (name, email, email_verified_at, password, created_at, updated_at, role, balance) VALUES ('John Doe', 'admin@com', NOW(), '$2y$10$m3EUqH2kXZzV9hCpKAZxuugerdqph4Jnauzp3LDq5rA5Y5Ir.k/Ga', NOW(), NOW(), 'admin', 100);
+INSERT INTO products (id, name, description, image, price, created_at, updated_at) VALUES (NULL, 'TV', 'Best TV', 'game.png', '1000', '2021-10-01
+00:00:00', '2021-10-01 00:00:00');
+INSERT INTO products (id, name, description, image, price, created_at, updated_at) VALUES (NULL, 'iPhone', 'Best iPhone', 'safe.png', '999', '2021-10-
+01 00:00:00', '2021-10-01 00:00:00');
+INSERT INTO products (id, name, description, image, price, created_at, updated_at) VALUES (NULL, 'Chromecast', 'Best Chromecast', 'submarine.png',
+'30', '2021-10-01 00:00:00', '2021-10-01 00:00:00');
+INSERT INTO products (id, name, description, image, price, created_at, updated_at) VALUES (NULL, 'Glasses', 'Best Glasses', 'game.png', '100', '2021-
+10-01 00:00:00', '2021-10-01 00:00:00');
